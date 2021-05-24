48f18f718bb8f25a81f8a52e647b8f7ca575333d
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-24 15:55:31 +0530

    Create ComplexNumbersUse.java

---
+++ Lecture 1 : OOPS 1/ComplexNumbersUse.java
@@ -0,0 +1,14 @@
+public class ComplexNumbersUse {
+  
+
+	public static void main(String[] args) {
+		
+		ComplexNumbers c=new ComplexNumbers(5, 6);
+		c.multiply(new ComplexNumbers(1, 1));
+		c.conjugate().print();
+    
+    
+		
+	}
+
+}
