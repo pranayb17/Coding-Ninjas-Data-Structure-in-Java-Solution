f8c393116f0d2d730f70cc16665dbef21ad34412
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-25 12:09:24 +0530

    Create VehicleUse.java

---
+++ Lecture 2 : OOPS 2/Component of OOPS/VehicleUse.java
@@ -0,0 +1,12 @@
+public class VehicleUse {
+
+	public static void main(String[] args) {
+		Vehicle v = new Vehicle();
+		v.print();
+		
+		Car c = new Car();
+		c.numGears = 10;
+		c.color = "Black";
+		c.print();
+	}
+}
