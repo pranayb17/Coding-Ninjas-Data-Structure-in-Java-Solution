0e62b8db2ec8b9a2f1dfcc3f023f334aca39c6ab
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-25 15:18:14 +0530

    Create CheckPalindromeRrecursive.java

---
+++ Lecture 3: Recursion 1/CheckPalindromeRrecursive.java
@@ -0,0 +1,49 @@
+// Check Palindrome (recursive)
+// Send Feedback
+// Check whether a given String S is a palindrome using recursion. Return true or false.
+// Input Format :
+// String S
+// Output Format :
+// 'true' or 'false'
+// Constraints :
+// 0 <= |S| <= 1000
+// where |S| represents length of string S.
+// Sample Input 1 :
+// racecar
+// Sample Output 1:
+// true
+// Sample Input 2 :
+// ninja
+// Sample Output 2:
+// false
+
+
+
+public class solution {
+
+	  public static boolean isStringPalindrome(String input) {
+		
+          
+        if(input==null)         return false;     
+		
+		if(input.length()<=1)   return true;
+		
+		
+		
+
+        char first= input.charAt(0);
+		char last= input.charAt(input.length()-1);
+		
+		if(first!=last) return false;
+          
+          
+		else {
+			
+		return isStringPalindrome(input.substring(1,input.length()-1));
+		
+		}
+		
+		
+	}
+	
+}
