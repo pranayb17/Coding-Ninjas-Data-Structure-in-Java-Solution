46bab630fe07492dd8aa3698944040e310e21fe1
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-25 15:49:30 +0530

    Create StringToInt.java

---
+++ Assignment: Recursion 1/StringToInt.java
@@ -0,0 +1,42 @@
+// String to Integer
+// Send Feedback
+// Write a recursive function to convert a given string into the number it represents. That is input will be a numeric string that contains only numbers, you need to convert the string into corresponding integer and return the answer.
+// Input format :
+// Numeric string S (string, Eg. "1234")
+// Output format :
+// Corresponding integer N (int, Eg. 1234)
+// Constraints :
+// 0 <= |S| <= 9
+// where |S| represents length of string S.
+// Sample Input 1 :
+// 00001231
+// Sample Output 1 :
+// 1231
+// Sample Input 2 :
+// 12567
+// Sample Output 2 :
+// 12567
+
+
+
+public class solution {
+
+	public static int convertStringToInt(String input){
+    
+	        int len = input.length();
+	       
+	        
+	        if (len==1){
+	           return input.charAt(0) - '0';
+	        }
+	        
+	
+	        int smallOutput = convertStringToInt(input.substring(0,len-1));
+            int lastDigit = input.charAt(len-1) - '0';
+	        
+	        return smallOutput *10 + lastDigit;
+	        
+
+		}
+}
+
