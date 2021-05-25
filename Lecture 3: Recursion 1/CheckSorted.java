19950de3b82f0cdac8c15430883d0e46fe42abe7
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-25 12:56:25 +0530

    Update CheckSorted.java

--- Lecture 3: Recursion 1/CheckSorted.java
+++ Lecture 3: Recursion 1/CheckSorted.java
@@ -46,11 +46,25 @@ public static boolean checkSorted_2(int input[]){
 
 }
 
+// This function checks if the array is sorted from startIndex to end
+public static boolean checkSortedBetter(int input[],int startIndex){
+	
+	if(startIndex >= input.length - 1){
+		return true;
+	}
+	if(input[startIndex] > input[startIndex + 1]){
+		return false;
+	}
+	boolean smallAns = checkSortedBetter(input, startIndex + 1);
+	return smallAns;
+	
+}
+
 
 
 	public static void main(String[] args) {
 		int input[] = {1,2,3};
-		System.out.println(checkSorted_2(input));
+		System.out.println(checkSortedBetter(input,0));
 
 	}
 
