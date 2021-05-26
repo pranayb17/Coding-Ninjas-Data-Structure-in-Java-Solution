9023ab401dfb6ecff6804d8685612497eb5c7827
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-26 13:57:33 +0530

    Update MaximumProfit.java

--- DS Test 1/MaximumProfit.java
+++ DS Test 1/MaximumProfit.java
@@ -33,18 +33,21 @@ import java.util.Arrays;
 public class solution {
 
 	public static int maximumProfit(int budget[]) {
+		
 		Arrays.sort(budget);
 		int cost[] = new int [budget.length];
 		
 		for(int i = 0; i < budget.length; i++) {
-			cost[i] = budget[i] * (budget.length - i);
+		     cost[i] = budget[i] * (budget.length - i);
 		}
 		
 		int max = Integer.MIN_VALUE;
+		
+		
 		for(int i : cost) {
 			
 			if(i > max) {
-				max = i;
+			  max = i;
 			}
 		} return max;
     }   
