3629bf5ca311c4d22ff688334998dd47f27814f8
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-26 13:58:49 +0530

    Update MaximumProfit.java

--- DS Test 1/MaximumProfit.java
+++ DS Test 1/MaximumProfit.java
@@ -1,7 +1,8 @@
 // Maximum Profit on App
 
 // Send Feedback
-// You have made a smartphone app and want to set its subscription price such that the profit earned is maximised. There are certain users who will subscribe to your app only if their budget is greater than or equal to your price.
+// You have made a smartphone app and want to set its subscription price such that the profit earned is maximised. 
+// There are certain users who will subscribe to your app only if their budget is greater than or equal to your price.
 // You will be provided with a list of size N having budgets of subscribers and you need to return the maximum profit that you can earn.
 // Lets say you decide that price of your app is Rs. x and there are N number of subscribers. So maximum profit you can earn is :
 //  m * x
