2f890b2423a2de898522b978c78f16b893d69429
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-01 18:22:35 +0530

    Create ReplaceNodeWithDepth.java

---
+++ Lecture 10 : Trees/ReplaceNodeWithDepth.java
@@ -0,0 +1,61 @@
+// Replace node with depth
+// Send Feedback
+// In a given Generic Tree, replace each node with its depth value. You need to just update the data of each node, no need to return or print anything.
+// Input format :
+
+// Line 1 : Elements in level order form separated by space (as per done in class). Order is -
+
+// Root_data, n (No_Of_Child_Of_Root), n children, and so on for every element
+
+// Sample Input 1 :
+// 10 3 20 30 40 2 40 50 0 0 0 0 
+// Sample Output 1 : (Level wise, each level in new line)
+// 0 
+// 1 1 1 
+// 2 2
+
+
+
+public class Solution {
+
+/*	TreeNode structure 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
+    	public static void replaceWithDepth(TreeNode<Integer> root,int Depth){
+            
+            if(root == null) {
+                return;
+            }
+            
+            root.data = Depth++;
+            
+            
+            for(TreeNode<Integer> child: root.children){
+            
+            replaceWithDepth( child, Depth);
+                
+            }  
+        }
+
+    
+    
+    
+	public static void replaceWithDepthValue(TreeNode<Integer> root){
+
+        int Depth = 0;
+        
+        replaceWithDepth(root, Depth);
+		
+	}
+	
+	
+}
