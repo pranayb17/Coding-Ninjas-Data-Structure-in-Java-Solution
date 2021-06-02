7300d661db9f93896281ba8b0a40fe2f1c161276
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-02 15:04:24 +0530

    Add files via upload

---
+++ Lecture 11: Binary Trees/Diameter Binary Tree/BinaryTreeUse.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class BinaryTreeUse {
+
+	public static void printTree(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return;
+		}
+		String toBePrinted = root.data + "";
+		if (root.left != null) {
+			toBePrinted += "L:" + root.left.data + ",";
+		}
+		
+		if (root.right != null) {
+			toBePrinted += "R:" + root.right.data;
+		}
+		System.out.println(toBePrinted);
+		printTree(root.left);
+		printTree(root.right);
+	}
+	
+	public static BinaryTreeNode<Integer> takeInput(Scanner s) {
+		int rootData;
+		System.out.println("Enter root data");
+		rootData = s.nextInt();
+		if (rootData == -1) {
+			return null;
+		}
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		root.left = takeInput(s);
+		root.right = takeInput(s);
+		return root;
+	}
+	
+	public static BinaryTreeNode<Integer> takeInputLevelWise() {
+		Scanner s = new Scanner(System.in);
+		QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+		System.out.println("Enter root data");
+		int rootData = s.nextInt();
+		if (rootData == -1) {
+			return null;
+		}
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> front;
+			try {
+				front = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			System.out.println("Enter left child of " + front.data);
+			int leftChild = s.nextInt();
+			if (leftChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(leftChild);
+				pendingNodes.enqueue(child);
+				front.left = child;
+			}
+			
+			System.out.println("Enter right child of " + front.data);
+			int rightChild = s.nextInt();
+			if (rightChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(rightChild);
+				pendingNodes.enqueue(child);
+				front.right = child;
+			}
+		}
+		return root;
+	}
+	
+	public static int countNodes(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return 0;
+		}
+		int ans = 1;
+		ans += countNodes(root.left);
+		ans += countNodes(root.right);
+		return ans;
+	}
+	
+	public static void mirror(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return;
+		}
+		mirror(root.left);
+		mirror(root.right);
+		BinaryTreeNode<Integer> temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+		return;
+	}
+	
+	public static int diameter(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return 0;
+		}
+		
+		int option1 = height(root.left) + height(root.right);
+		int option2 = diameter(root.left);
+		int option3 = diameter(root.right);
+		return Math.max(option1, Math.max(option2, option3));
+	}
+	
+	public static int height(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return 0;
+		}
+		int lh = height(root.left);
+		int rh = height(root.right);
+		return 1 + Math.max(lh, rh);
+	}
+
+	
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		printTree(root);
+		System.out.println("diameter:" + diameter(root));
+	}
+}
