e6d61b51c966f9ef5dc299be4c6e63f35cdca5dc
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-08 16:54:40 +0530

    Create Student.java

---
+++ Lecture 1 : OOPS 1/static | final | this keyword/Student.java
@@ -0,0 +1,45 @@
+package classes_and_objects;
+
+public class Student {
+	public String name;
+	// final data members can be initialized as soon as they are declared
+	private final int rollNumber;
+	
+	private static int numStudents;
+	
+//	public Student() {
+//		rollNumber = 100;
+//	}
+	
+//	public Student(String n) {
+//		name  = n;
+//		rollNumber = 100;
+//	}
+	
+	public static int getNumStudents() {
+		return numStudents;
+	}
+	
+	public Student(String name, int rollNumber) {
+		//System.out.println(this);
+		this.name = name;
+		this.rollNumber = rollNumber;
+		numStudents++;
+	}
+	
+//	public void setRollnumber(int rn) {
+//		if (rn <= 0) {
+//			return;
+//		}
+//		rollNumber = rn;
+//	}
+	
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	
+	public void print() {
+		System.out.println(name + " : " + rollNumber);
+	}
+	
+}
