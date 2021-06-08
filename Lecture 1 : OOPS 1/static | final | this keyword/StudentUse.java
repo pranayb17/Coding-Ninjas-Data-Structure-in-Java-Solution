0750902ede7797a9bfdf367218cc89e35eaa4fc3
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-08 16:55:08 +0530

    Add files via upload

---
+++ Lecture 1 : OOPS 1/static | final | this keyword/StudentUse.java
@@ -0,0 +1,33 @@
+package classes_and_objects;
+
+import java.util.Scanner;
+
+public class StudentUse {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		Student s1 = new Student("Ankush", 123);
+		//System.out.println(s1);
+//		System.out.println(s1.name);
+//		System.out.println(s1.getRollNumber());
+		s1.print();
+		
+		Student s2 = new Student("Manisha", 121);
+		//System.out.println(s2);
+		s2.print();
+		
+//		System.out.println(s1.numStudents);
+//		System.out.println(s2.numStudents);
+		System.out.println(Student.getNumStudents());
+		//System.out.println(s1.getNumStudents());
+		
+		System.out.println(s1.name);
+		
+//		System.out.println(s1.name);
+//		System.out.println(s2.name);
+//		System.out.println(s1.getRollNumber());
+//		//System.out.println(s1);
+	}
+
+}
