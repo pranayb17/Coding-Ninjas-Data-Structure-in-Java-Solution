71bd2d713528121aa9a5f50b7cfc6932ed5ab488
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-11 10:15:12 +0530

    Update FirstIndexOfNumber.java

--- Lecture 3: Recursion 1/FirstIndexOfNumber.java
+++ Lecture 3: Recursion 1/FirstIndexOfNumber.java
@@ -21,24 +21,87 @@
 
 
 
+
 public class Solution {
+    
+//     public static int firstIndex(int input[] , int x) {
+        
+//         if(input.length == 0) {
+//             return -1;
+//         }
+        
+//         if(input[0] == x){
+//             return 0;
+//         }
+        
+//         int smallArray[] = new int [input.length - 1];
+//         for(int i = 1; i < input.length; i++){
+//             smallArray[i - 1] = input[i];
+//         }
+        
+//         int fi = firstIndex(smallArray, x);
+//         if(fi == -1){
+//             return -1;
+//         }else{
+//             return fi + 1;
+//         }
+//     }
+    
+    
+    
+    
+     public static int firstIndexBetter(int input[] , int x, int si) {
+         
+         if(si == input.length){
+             return -1;
+         }
+         
+         if(input[si] == x){
+             return si;
+         }
+         
+         int k = firstIndexBetter(input, x, si + 1);
+         
+         return k;
+         
+         
+     }
+    
+    
+    
+    
+    
+    public static int firstIndex(int input[] , int x) {
+        
+        return firstIndexBetter(input, x, 0);
+        
+    }
+    
+    
+    
+    
+    
+     
+    
+    
+    
 	
-    private  static int firstIndex(int input[],int StartIndex,int x) {
+//     private  static int firstIndex(int input[],int StartIndex,int x) {
         
-        if(input[StartIndex] == x) {
-            return StartIndex;
-        }    
+//         if(input[StartIndex] == x) {
+//             return StartIndex;
+//         }    
         
-        if(StartIndex >= input.length - 1  &&  input[StartIndex] != x ) {
-            return -1;
-        }
+// 		if(StartIndex >= input.length - 1 && input[StartIndex] != x ) {
+//             return -1;
+//         }
 		
-	int SmallAns = firstIndex(input, StartIndex + 1, x);
-	   return SmallAns;
-	}  
+// 		int SmallAns = firstIndex(input, StartIndex + 1, x);
+// 			return SmallAns;
+// 		}  
 		
-	public static int firstIndex(int input[] , int x) {
-	  return firstIndex(input, 0, x);
-	 }
+// 	    public static int firstIndex(int input[] , int x) {
+// 		    return firstIndex(input, 0, x);
+// 	 }
 	
 }
