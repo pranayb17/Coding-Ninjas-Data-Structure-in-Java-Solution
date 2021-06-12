69838f8deb1c31454dc22da72a97d00313368548
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-12 13:35:38 +0530

    Update RemoveDuplicatesRec.java

--- Lecture 4: Recursion 2/RemoveDuplicatesRec.java
+++ Lecture 4: Recursion 2/RemoveDuplicatesRec.java
@@ -21,22 +21,18 @@
 public class Solution {
 
 	public static String removeConsecutiveDuplicates(String s) {
+		 
+                if(s.length() == 1) {
+                    return s;
+                }
 		
-        
-        int len=s.length();
-        
-        if(len==1)  return s;
-		
-		if(s.charAt(0)==s.charAt(1)) {
-			return removeConsecutiveDuplicates(s.substring(1));
+		if(s.charAt(0) == s.charAt(1)) {
+		    return removeConsecutiveDuplicates(s.substring(1));
 		}
 		
 		else {
-			
-			return s.charAt(0)+removeConsecutiveDuplicates(s.substring(1));
-        }
+		    return s.charAt(0) + removeConsecutiveDuplicates(s.substring(1));
+                }
 	
-    
-    
-    }
+       }
 }
