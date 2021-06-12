be80bfe14054ad54f086ea31a40adc4fb9e6f262
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-12 13:31:02 +0530

    Update replaceCharacterRec.java

--- Lecture 4: Recursion 2/replaceCharacterRec.java
+++ Lecture 4: Recursion 2/replaceCharacterRec.java
@@ -21,11 +21,11 @@ public class Solution {
 	public static String replaceCharacter(String input, char c1, char c2) {
 		
 
-                 if(input.length()==0) {
+                 if(input.length() == 0) {
 			return input;
 		}
 		
-		if(input.charAt(0)==c1) {
+		if(input.charAt(0) == c1) {
 			return c2+ replaceCharacter(input.substring(1), c1, c2);
 		}
 		
