6ad91464daa42370a20fb69271388aa68897b9e8
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-14 20:04:14 +0530

    Update Staircase.java

--- Lecture 4: Recursion 2/Staircase.java
+++ Lecture 4: Recursion 2/Staircase.java
@@ -19,30 +19,30 @@
 
 
 
+
 public class Solution {
 	
 
     public static int staircase(int n){
     	
-    	if(n == 0 || n == 1)  {
-            return 1;
+    	if(n < 0) {
+            return 0;
         }
     	
-    	else if(n == 2)  {
-              return 2;
+    	if(n == 0) {
+            return 1;
         } 
     	
-    	 int x = staircase(n-1);
-         int y = staircase(n-2);
-         int z = staircase (n-3);
+    	 int x = staircase(n - 1);
+         int y = staircase(n - 2);
+         int z = staircase(n - 3);
          
          
-           int count = x + y + z;
-           
-           return count;
+         int count = x + y + z;
+          
+         return count;
          
 
 	}
 	
 }
-
