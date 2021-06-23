8fc652718024ed71bcc4ac36ddbec17b08bd0554
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-23 21:44:57 +0530

    Create LLcollections.java

---
+++ Lecture 8 : Linked List 2/LLcollections.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class collectionsLinkedList {
+  
+  public static void main(String[] args){
+    
+    LinkedList<Integer> list = new LinkedList<>();  //by default doubly linked list
+    
+    list.add(10);
+    list.add(20);
+    list.addFirst(40);
+    list.addFirst(70);
+    list.set(index, element);
+    
+    
+    System.out.println(list.size());
+    
+    for(int i =0; i < list.size(); i++){
+      System.out.println(list.get(i) + " ");
+    }
+    
+  }
+  
+}
